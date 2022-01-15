7aae70dcc42fb69ed2d4e08b17531542dc6084b9
Matt <carlotta.matt@gmail.com>
2022-01-14 20:34:41 -0800

    Clean up nodemon config

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,6 @@
 import typescript from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
 
-const terserOptions = {
-  compress: {
-    warnings: false,
-    comparisons: false,
-    inline: 2
-  },
-  mangle: {
-    safari10: true
-  },
-  output: {
-    comments: false,
-    ascii_only: true
-  }
-}
-
 export default [
   {
     preserveModules: true,
@@ -32,7 +17,20 @@ export default [
     ],
     plugins: [
       typescript({ tsconfig: './tsconfig.prod.json' }),
-      terser(terserOptions)
+      terser({
+        compress: {
+          warnings: false,
+          comparisons: false,
+          inline: 2
+        },
+        mangle: {
+          safari10: true
+        },
+        output: {
+          comments: false,
+          ascii_only: true
+        }
+      })
     ]
   }
 ]
