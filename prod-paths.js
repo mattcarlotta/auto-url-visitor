15924a1faa7ec789f6b0fa050d7488f27be3ed48
Matt <carlotta.matt@gmail.com>
2022-01-14 20:11:56 -0800

    Remove prod paths

--- prod-paths.js
+++
@@ -1,7 +0,0 @@
-const tsconfigpaths = require('tsconfig-paths')
-const { compilerOptions } = require('./tsconfig.json')
-
-tsconfigpaths.register({
-  baseUrl: compilerOptions.outDir,
-  paths: compilerOptions.paths
-})
